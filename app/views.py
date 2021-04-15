28cf6289ba0d7ec789d2070a89aa85e6676a396a
EitaIchinose <dellps.psdell@gmail.com>
2021-04-15 14:12:24 +0900

    ビュー実装

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Post
 
-# Create your views here.
+# Viewを継承して、クラスベース汎用ビューを作成
+class IndexView(View):
+  def get(self, request, *args, **kwargs):       # get関数は画面が表示されたら必ず最初に呼ばれる
+    post_data = Post.objects.order_by('-id')     # Postモデルを呼び出し、降順に並び替えている
+    return render(request, 'app/index.html', {   # render関数を使用して、テンプレートにデータを渡します
+      'post_data': post_data,
+    })
