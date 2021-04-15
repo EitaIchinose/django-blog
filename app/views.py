6bad2f45edf6246e66bf484c11e2c53e4475b4e1
EitaIchinose <dellps.psdell@gmail.com>
2021-04-15 19:22:35 +0900

    投稿編集機能実装

--- app/views.py
+++ app/views.py
@@ -12,12 +12,14 @@ class IndexView(View):
       'post_data': post_data,
     })
     
+    
 class PostDetailView(View):
     def get(self, request, *args, **kwargs):
       post_data = Post.objects.get(id=self.kwargs['pk'])
       return render(request, 'app/post_detail.html', {
         'post_data': post_data,
       })
+      
       
 class CreatePostView(LoginRequiredMixin, View):
   def get(self, request, *args, **kwargs):
@@ -37,6 +39,37 @@ class CreatePostView(LoginRequiredMixin, View):
       post_data.content = form.cleaned_data['content'] # フォームで入力された内容のデータを代入
       post_data.save()   # データベースへ保存
       return redirect('post_detail', post_data.id)     # 詳細画面へリダイレクト
+    
+    return render(request, 'app/post_form.html', {
+      'form': form
+    })
+    
+    
+class PostEditView(LoginRequiredMixin, View):
+  def get(self, request, *args, **kwargs):
+    post_data = Post.objects.get(id=self.kwargs['pk'])
+    form = PostForm(
+      request.POST or None,
+      initial = {
+        'title': post_data.title,
+        'content': post_data.content
+      }
+    )
+    
+    return render(request, 'app/post_form.html', {
+      'form': form
+    })
+
+  def post(self, request, *args, **kwargs):
+    form = PostForm(request.POST or None)
+    
+    if form.is_valid():
+      post_data = Post.objects.get(id=self.kwargs['pk'])
+      post_data.author = request.user
+      post_data.title = form.cleaned_data['title']
+      post_data.content = form.cleaned_data['content']
+      post_data.save()
+      return redirect('post_detail', self.kwargs['pk'])
     
     return render(request, 'app/post_form.html', {
       'form': form
