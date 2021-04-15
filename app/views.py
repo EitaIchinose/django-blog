92446c11c18254245c197729f0a624cebb7d6d9f
EitaIchinose <dellps.psdell@gmail.com>
2021-04-15 18:07:59 +0900

    新規投稿機能実装

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Viewを継承して、クラスベース汎用ビューを作成
 class IndexView(View):
@@ -16,3 +18,26 @@ class PostDetailView(View):
       return render(request, 'app/post_detail.html', {
         'post_data': post_data,
       })
+      
+class CreatePostView(LoginRequiredMixin, View):
+  def get(self, request, *args, **kwargs):
+    form = PostForm(request.POST or None)
+    
+    return render(request, 'app/post_form.html', {
+      'form': form
+    })
+    
+  def post(self, request, *args, **kwargs):
+    form = PostForm(request.POST or None)
+    
+    if form.is_valid():  # フォームの内容を確認
+      post_data = Post() # Postモデルをpost_dataへ格納
+      post_data.author = request.user  # ログインユーザーをauthorへ代入
+      post_data.title = form.cleaned_data['title']     # フォームで入力されたタイトルのデータを代入
+      post_data.content = form.cleaned_data['content'] # フォームで入力された内容のデータを代入
+      post_data.save()   # データベースへ保存
+      return redirect('post_detail', post_data.id)     # 詳細画面へリダイレクト
+    
+    return render(request, 'app/post_form.html', {
+      'form': form
+    })
